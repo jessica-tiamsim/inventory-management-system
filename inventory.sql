8c8b6ab6ff0c98f3afb7fb99eca2f8be814b784c
Wren_Elison <2301726@ub.edu.ph>
2026-05-04 23:38:51 +0800

    First_Fix_User_Table

--- inventory.sql
+++ inventory.sql
@@ -2,11 +2,11 @@ CREATE DATABASE db;
 USE db;
 
 CREATE TABLE PRODUCTS (
-	user_id INT,
-    username VARCHAR(255),
-    email VARCHAR(255),
+	user_id INT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
     password_hash VARCHAR(255),
-    role ENUM('Admin', 'Staff'),
+    role ENUM('admin', 'staff'),
     user_is_active BOOLEAN,
     user_created_at DATETIME,
     user_updated_at DATETIME
