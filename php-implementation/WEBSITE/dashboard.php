91d8e46edf10c334e551ff4480c75149041c9e74
S-2102482 <2102482@ub.edu.ph>
2026-05-06 22:25:42 +0800

    dashboard layout (sidebar, header and body) first commit

---
+++ php-implementation/WEBSITE/dashboard.php
@@ -0,0 +1,50 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel = "stylesheet" href = "general.css"/>
+
+</head>
+<body>
+    <aside class = "sidebar">
+        <div class = "icon">
+            PRISM
+        </div>
+        <div class = "navigation">
+            <button class = "button-navigation">Dashboard</button>
+            <button class = "button-navigation">Products</button>
+            <button class = "button-navigation">Stock Movements</button>
+            <button class = "button-navigation">Reports</button>
+            <button class = "button-navigation">Users</button>
+        </div>
+        <div class = "logout">
+            <button>sda</button>
+        </div>
+    </aside>
+
+    <div class = "content">
+        <div class = "header"> 
+            <div>PRISM | Inventory Management System</div>
+            <div>Alex Reyes</div>
+        </div>
+
+        <div class = "body">
+            <div class = "welcome">
+                
+            </div>
+            <div class = "KPI">
+
+            </div>
+            <div class = "activities">
+                <div></div>
+                <div></div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
