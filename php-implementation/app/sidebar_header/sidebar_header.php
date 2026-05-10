5e2c4449eeb3d6c46124aebf2756071004b7622f
S-2102482 <2102482@ub.edu.ph>
2026-05-10 22:31:48 +0800

    redesing of sidebar and header to be used and include in all pages

---
+++ php-implementation/app/sidebar_header/sidebar_header.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PRISM</title>
+   <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+   <link rel="stylesheet" href="sidebar_header.css">
+</head>
+<body>
+   <aside>
+      <div class= "logo">
+         <div class="logo1">
+            <img src= "C:\xampp\htdocs\inventory-management-system\php-implementation\image.png" class="prism_image">
+            <h1>PRISM</h1>
+         </div>
+         <div class="logo_user">
+            <p>Admin Portal</p>
+         </div>
+      </div>
+      
+      <nav>
+         <a href="#" class="active">
+            <img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png">
+            <p>Dashboard</p>
+         </a>
+         <a href="#">
+            <img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png">
+            <p>Products</p>
+         </a>
+         <a href="#">
+            <img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png">
+            <p>Stock Movements</p>
+         </a>
+         <a href="#">
+            <img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png">
+            <p>Reports</p>
+         </a>
+         <a href="#">
+            <img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png">
+            <p>Profile</p>
+         </a>
+      </nav>
+
+      <div class ="user-panel">
+         <div class="user">
+            <div class="profile">
+               <img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png" class="prism_image">
+            </div>
+            <div class="status">
+               <p>Admin User</p>
+               <p id=admin>Admin</p>
+            </div>
+         </div>
+         <button href="#">
+            <img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png" class="logout_image">
+            <p id=logout>Logout</p>
+         </button>
+      </div>
+   </aside>
+   <div class="main">
+   <header>
+      <div class= "upper">
+         <div class= "left">
+            <!--<img src="C:\xampp\htdocs\inventory-management-system\php-implementation\image.png">-->
+            <div class="shape">asd</div>
+            <p>PRISM | Inventory Management</p>
+         </div>
+         <div class="right">
+            <div class = "admin">
+               <p>Admin</p>
+            </div>
+            <div class = "alex">
+               <p>Alex Reyes</p>
+            </div> 
+         </div>
+      </div>
+      <div class="lower">
+         <p>PRISM / INVENTORY MANAGEMENT / DASHBOARD</p>
+      </div>
+   </header>
+   <div class="content">
+   </div>
+
+   </div>
+ </body>
+</html>
+
