43b256df1a7c88666a127658847ab8fab8c3a808
Wren_Elison <2301726@ub.edu.ph>
2026-05-26 21:13:11 +0800

    model_fix

--- js-implementation/src/models/userModel.js
+++ js-implementation/src/models/userModel.js
@@ -1,16 +1,7 @@
-const pool = require('../../config/db');
+const db = require('../../config/db');
 
-const UserModel = {
-    // Looks up user rows if either the unique username OR the email column matches
-    async findByUsernameOrEmail(identifier) {
-        const query = `
-            SELECT * FROM users 
-            WHERE username = ? OR email = ? 
-            LIMIT 1
-        `;
-        const [rows] = await pool.execute(query, [identifier, identifier]);
-        return rows[0] || null;
-    }
+exports.findByCredentials = async (identifier) => {
+    const query = 'SELECT * FROM users WHERE username = ? OR email = ? LIMIT 1';
+    const [rows] = await db.execute(query, [identifier, identifier]);
+    return rows[0];
 };
-
-module.exports = UserModel;
