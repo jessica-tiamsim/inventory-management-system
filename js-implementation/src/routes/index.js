108d47971c0f674d4cb77a4eda1f0bd7652b3579
Wren_Elison <2301726@ub.edu.ph>
2026-05-28 22:32:22 +0800

    navigation_fix

--- js-implementation/src/routes/index.js
+++ js-implementation/src/routes/index.js
@@ -2,19 +2,27 @@
 const express = require('express');
 const router = express.Router();
 
-// 1. Import your new modular route files
+// 1. Import your modular route files
 const loginRoutes = require('./loginRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const productsRoutes = require('./productsRoutes');
+const profileRoutes = require('./profileRoutes');
+const stockMovementRoutes = require('./stockmovementRoutes');
+
 
 // 2. Delegate the traffic!
-// Anything related to auth goes to login.js
+// Anything related to auth (login, logout) goes to loginRoutes.js
 router.use('/', loginRoutes); 
+
+// Anything related to employee management (/profile, /users/add) goes to profileRoutes.js
+router.use('/', profileRoutes); 
 
 // Anything starting with /dashboard goes to dashboardRoutes.js
 router.use('/dashboard', dashboardRoutes); 
 
 // Anything starting with /products goes to productsRoutes.js
 router.use('/products', productsRoutes); 
+
+router.use('/stock_movement', stockMovementRoutes); 
 
 module.exports = router;
